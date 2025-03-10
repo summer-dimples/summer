d14e0776173606bbb21cf063a7471c4bcefe284b
Jinxun Chen <gggary003@gmail.com>
2025-03-10 12:27:25 +0000

    3/10 12：27

--- Long_Model.py
+++ Long_Model.py
@@ -389,7 +389,7 @@ def plot_vehicle_ratio(ratio_results):
     flow_values = [result[3] for result in ratio_results]
     velocity_values = [result[4] for result in ratio_results]
     
-    fig, axs = plt.subplots(1, 2, figsize=(20, 10))
+    fig, axs = plt.subplots(1, 2, figsize=(25, 12))
     
     corners = np.array([[0, 0], [1, 0], [0.5, 0.75**0.5]])
     triangle = tri.Triangulation(corners[:, 0], corners[:, 1])
@@ -412,13 +412,13 @@ def plot_vehicle_ratio(ratio_results):
     cbar1.set_label('Flow', fontsize=16)
     
     # adding labels
-    axs[0].text(corners[0,0]-0.1, corners[0,1]-0.05, 'Lorry (100%)', fontsize=14)
-    axs[0].text(corners[1,0]-0.05, corners[1,1]-0.05, 'Regular (100%)', fontsize=14)
-    axs[0].text(corners[2,0]-0.05, corners[2,1]+0.05, 'Fast (100%)', fontsize=14)
+    axs[0].text(corners[0,0]-0.1, corners[0,1]-0.05, 'Lorry (100%)', fontsize=20)
+    axs[0].text(corners[1,0]-0.05, corners[1,1]-0.05, 'Regular (100%)', fontsize=20)
+    axs[0].text(corners[2,0]-0.05, corners[2,1]+0.05, 'Fast (100%)', fontsize=20)
     
     # adding flow values
     for (x, y, flow) in zip(x_values, y_values, flow_values):
-        axs[0].text(x, y+0.02, f'{flow:.2f}', ha='center', fontsize=14)
+        axs[0].text(x, y+0.02, f'{flow:.2f}', ha='center', fontsize=16)
 
     axs[0].set_xlim(-0.1, 1.1)
     axs[0].set_ylim(-0.1, 0.9)
@@ -434,31 +434,31 @@ def plot_vehicle_ratio(ratio_results):
 
     
     # adding labels
-    axs[1].text(corners[0,0]-0.1, corners[0,1]-0.05, 'Lorry (100%)', fontsize=14)
-    axs[1].text(corners[1,0]-0.05, corners[1,1]-0.05, 'Regular (100%)', fontsize=14)
-    axs[1].text(corners[2,0]-0.05, corners[2,1]+0.05, 'Fast (100%)', fontsize=14)
+    axs[1].text(corners[0,0]-0.1, corners[0,1]-0.05, 'Lorry (100%)', fontsize=20)
+    axs[1].text(corners[1,0]-0.05, corners[1,1]-0.05, 'Regular (100%)', fontsize=20)
+    axs[1].text(corners[2,0]-0.05, corners[2,1]+0.05, 'Fast (100%)', fontsize=20)
     
     # adding velocity values
     for (x, y, vel) in zip(x_values, y_values, velocity_values):
-        axs[1].text(x, y+0.02, f'{vel:.2f}', ha='center', fontsize=14)
+        axs[1].text(x, y+0.02, f'{vel:.2f}', ha='center', fontsize=16)
 
     axs[1].set_xlim(-0.1, 1.1)
     axs[1].set_ylim(-0.1, 0.9)
     axs[1].axis('off')
      
     plt.tight_layout(pad=5.0, w_pad=4.0)
-    plt.savefig('vehicle_ratio_ternary.png')
+    plt.savefig('vehicle_ratio_ternary.png', dpi=600)
     
     return
 
 # sensitive analysis
 def run_sensitivity_analysis():
     
-    length = 1500   
-    t0 = 1500       
-    steps = 5000   
+    length = 10   
+    t0 = 10       
+    steps = 10  
     base_target_density = 0.2  
-    interval_size = 50 
+    interval_size = 1
 
     # parameter ranges
     random_brake_params = np.linspace(0, 0.6, 20)  
@@ -608,70 +608,70 @@ def run_sensitivity_analysis():
         ratio_results.append((lorry_ratio, regular_ratio, fast_ratio, avg_flow, avg_velocities))
         print(f"Lorry={lorry_ratio:.1f}, Regular={regular_ratio:.1f}, Fast={fast_ratio:.1f}, Average flow = {avg_flow:.4f}, Average velocities = {avg_velocities:.4f}")
     
-    fig, axs = plt.subplots(2, 3, figsize=(24, 16))
+    fig, axs = plt.subplots(2, 3, figsize=(30, 20))
     
     # brake
     axs[0,0].plot(random_brake_params, random_brake_results, 'o-', label='Average Flow', color='red')
     axs[0,0].plot(random_brake_params, random_brake_velocities, 'o-', label='Average Velocities', color='blue')
-    axs[0,0].set_xlabel('Random Brake Probability')
-    axs[0,0].set_ylabel('Values')
-    axs[0,0].set_title('Impact of Random Brake Probability on Flow and Velocities')
+    axs[0,0].set_xlabel('Random Slow Down Probability', fontsize=20)
+    axs[0,0].set_ylabel('Values', fontsize=20)
+    axs[0,0].set_title('Impact of Random Slow Down Probability on Flow and Velocities', fontsize=22)
     axs[0,0].grid(True)
-    axs[0,0].legend()
+    axs[0,0].legend(fontsize=20)
     
     # overtake
     axs[0,1].plot(overtake_params, overtake_results, 'o-', label='Average Flow', color='red')
     axs[0,1].plot(overtake_params, overtake_velocities, 'o-', label='Average Velocities', color='blue')
-    axs[0,1].set_xlabel('Overtake Rate')
-    axs[0,1].set_ylabel('Values')
-    axs[0,1].set_title('Impact of Overtake Rate on Flow and Velocities')
+    axs[0,1].set_xlabel('Overtake Rate', fontsize=20)
+    axs[0,1].set_ylabel('Values', fontsize=20)
+    axs[0,1].set_title('Impact of Overtake Rate on Flow and Velocities', fontsize=22)
     axs[0,1].grid(True)
-    axs[0,1].legend()
+    axs[0,1].legend(fontsize=20)
 
     # vmax
     axs[0,2].plot(vmax_offsets, vmax_results, 'o-', label='Average Flow', color='red')
     axs[0,2].plot(vmax_offsets, vmax_velocities, 'o-', label='Average Velocities', color='blue')
-    axs[0,2].set_xlabel('Maximum Velocity Offset')
-    axs[0,2].set_ylabel('Values')
-    axs[0,2].set_title('Impact of Maximum Velocity on Flow and Velocities')
+    axs[0,2].set_xlabel('Maximum Velocity Offset', fontsize=20)
+    axs[0,2].set_ylabel('Values', fontsize=20)
+    axs[0,2].set_title('Impact of Maximum Velocity on Flow and Velocities', fontsize=22)
     axs[0,2].grid(True)
-    axs[0,2].legend()
+    axs[0,2].legend(fontsize=20)
 
     # density
     axs[1,0].plot(actual_densities, density_results, 'o-', label='Average Flow', color='red')
     axs[1,0].plot(actual_densities, density_velocities, 'o-', label='Average Velocities', color='blue')
-    axs[1,0].set_xlabel('Actual Density')
-    axs[1,0].set_ylabel('Values')
-    axs[1,0].set_title('Impact of Density on Flow and Velocities')
+    axs[1,0].set_xlabel('Actual Density', fontsize=20)
+    axs[1,0].set_ylabel('Values', fontsize=20)
+    axs[1,0].set_title('Impact of Density on Flow and Velocities', fontsize=22)
     axs[1,0].grid(True)
-    axs[1,0].legend()
+    axs[1,0].legend(fontsize=20)
 
     # boost
     axs[1,1].plot(boost_chance_params, boost_chance_results, 'o-', label='Average Flow', color='red')
     axs[1,1].plot(boost_chance_params, boost_chance_velocities, 'o-', label='Average Velocities', color='blue')
-    axs[1,1].set_xlabel('Boost Chance')
-    axs[1,1].set_ylabel('Values')
-    axs[1,1].set_title('Impact of Boost Chance on Flow and Velocities')
+    axs[1,1].set_xlabel('Boost Chance', fontsize=20)
+    axs[1,1].set_ylabel('Values', fontsize=20)
+    axs[1,1].set_title('Impact of Boost Chance on Flow and Velocities', fontsize=22)
     axs[1,1].grid(True)
-    axs[1,1].legend()
+    axs[1,1].legend(fontsize=20)
 
     # return
     axs[1,2].plot(return_rate_params, return_rate_results, 'o-', label='Average Flow', color='red')
     axs[1,2].plot(return_rate_params, return_rate_velocities, 'o-', label='Average Velocities', color='blue')
-    axs[1,2].set_xlabel('Return Rate')
-    axs[1,2].set_ylabel('Values')
-    axs[1,2].set_title('Impact of Return Rate on Flow and Velocities')
+    axs[1,2].set_xlabel('Return Rate', fontsize=20)
+    axs[1,2].set_ylabel('Values', fontsize=20)
+    axs[1,2].set_title('Impact of Return Rate on Flow and Velocities', fontsize=22)
     axs[1,2].grid(True)
-    axs[1,2].legend()
+    axs[1,2].legend(fontsize=20)
     
     
-    plt.tight_layout()
-    plt.savefig('sensitivity_analysis_results.png')
+    plt.tight_layout(pad=4.0)
+    plt.savefig('sensitivity_analysis_results.png', dpi=600)
     
     plot_vehicle_ratio(ratio_results)
 
-    return 
 
+    return 
 
 def compare_brake_chances():
     length = 1000   
@@ -791,6 +791,7 @@ def main():
     
     run_sensitivity_analysis()
     
+
     print("finished")
 
 if __name__ == "__main__":
